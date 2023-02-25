ec3b9b056ae6558301325f9be5e3a0931a425c24
AndresChavez <andres.cz.chavez@gmail.com>
2023-02-25 18:14:27 -0500

    Creación de Schemas de pokemon

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,20 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose/dist';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  // usualmente las clases son una abstración de la tabla de la base de datos
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
