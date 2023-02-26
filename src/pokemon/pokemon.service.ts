70f4c6367e8ab5b3c7712b1d56557389aa4c6494
AndresChavez <andres.cz.chavez@gmail.com>
2023-02-25 20:19:34 -0500

    Búsqueda generica por termino y manejo de errores

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { BadRequestException, InternalServerErrorException } from '@nestjs/common/exceptions';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common/exceptions';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
@@ -26,20 +30,40 @@ export class PokemonService {
         );
       }
       console.log(error);
-      throw new InternalServerErrorException(`Can't craete Pokemon - Check server logs`)
+      throw new InternalServerErrorException(
+        `Can't craete Pokemon - Check server logs`,
+      );
     }
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  async findAll() {}
+
+  async findOne(term: string) {
+    let pokemon: Pokemon;
+
+    if (!isNaN(+term)) {
+      // validando si es un número
+      pokemon = await this.pokemonModel.findOne({ no: term });
+    }
+
+    if (!pokemon && isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term);
+    }
+
+    if(!pokemon){
+      pokemon = await this.pokemonModel.findOne({name: term.toLowerCase().trim() });
+    }
+
+    if (!pokemon)
+      throw new NotFoundException(
+        `Pokemon with id, name or no ${term} no found`,
+      );
+
+    return pokemon;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
-  }
-
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  update(term: number, updatePokemonDto: UpdatePokemonDto) {
+    return `This action updates a #${term} pokemon`;
   }
 
   remove(id: number) {
